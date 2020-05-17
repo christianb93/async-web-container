afae38f88be1ba3d860bfaf38e54e9df5adeb0a2
christianb93 <me@unknown>
2020-05-17 17:38:01 +0200

    Added documentation for connection_made

--- aioweb/protocol.py
+++ aioweb/protocol.py
@@ -54,6 +54,10 @@ class HttpProtocol(asyncio.Protocol): # pylint: disable=too-many-instance-attrib
         self._headers = {}
 
     def connection_made(self, transport):
+        """
+        This callback is invoked by the transport when a connection is established. It creates a task
+        which handles all future requests received via this connection
+        """
         self._transport = transport
         logger.debug("Connection started, transport is %s", self._transport)
         #
